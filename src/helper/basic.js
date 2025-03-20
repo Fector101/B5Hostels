aa4ee9f3aacc55994b38dbae2251b2e146a5fd4e
Fabian <fabianjoseph063@gmail.com>
2025-03-20 19:01:37 +0100

    Update basic.js

--- src/helper/basic.js
+++ src/helper/basic.js
@@ -2,15 +2,17 @@ const jwt = require('jsonwebtoken')
 
 
 const verifyToken = (req, res, next) => {
-    const token = req.cookies.token;
-    if (!token) return res.status(401).json({ error: 'Access denied' });
+    const userInfo = req.cookies.userInfo;
+    if (!userInfo) return res.status(401).sendFile(path.join(__dirname, '../../public/pages/login.html'));
+    // if (!userInfo) return res.status(401).json({ error: 'Access denied' });
 
     try {
-        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        const verified = jwt.verify(userInfo, process.env.JWT_SECRET);
         req.user = verified;
         next();
     } catch (err) {
-        res.status(400).json({ error: 'Invalid token' });
+        // res.status(400).json({ error: 'Invalid token' });
+        res.status(400).sendFile(path.join(__dirname, '../../public/pages/login.html'));
     }
 };
 

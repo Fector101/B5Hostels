949b129d5ac3ccb0d0df02efc4af8c914b6e3c1f
Fabian <fabianjoseph063@gmail.com>
2025-03-21 19:38:06 +0100

    Update students.js

--- src/routes/students.js
+++ src/routes/students.js
@@ -47,7 +47,7 @@ router.get("/dashboard", verifyToken, (req, res) => {
         matric_no: userInfo.matric_no,
         level: userInfo.level,
         room: userInfo.room || "Nil",
-        date_booked: userInfo.days_left || "not booked",
+        date_booked: userInfo.days_left || 0,
         total_paid: userInfo.total_paid || "Nil"
     };
     res.render("dashboard", data);

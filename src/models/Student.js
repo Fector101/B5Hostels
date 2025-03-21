2bd50ca9b06b069aa55db0275b23280597c06bf7
Fabian <fabianjoseph063@gmail.com>
2025-03-21 20:33:13 +0100

    Update Student.js

--- src/models/Student.js
+++ src/models/Student.js
@@ -1,3 +1,4 @@
+
 const mongoose = require('mongoose');
 
 const studentSchema = new mongoose.Schema({
@@ -8,11 +9,16 @@ const studentSchema = new mongoose.Schema({
   gender: { type: String, required: true },
   level: { type: String, required: true },
 
-  total_paid: { type: Number, required: false },
+  total_paid: { type: Number, default: 0 },
   room: { type: String, required: false },
-  date_booked: { type: String, required: false }
+  date_booked: { type: String, required: false },
 
-
+  payments: [
+    {
+      amount: { type: Number, required: true },
+      date: { type: Date, default: Date.now },
+    }
+  ]
 }, { timestamps: true });
 
 module.exports = mongoose.model('Student', studentSchema);

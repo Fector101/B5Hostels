1b7f41ba2d9231c1051118946cc6b976d4a98d7f
fector101 <fabianjoseph063@gmail.com>
2025-03-23 09:33:21 +0100

    Enhance responsive design and improve admin-students functionality

--- public/js/admin-students.js
+++ public/js/admin-students.js
@@ -1,27 +1,27 @@
 
 const backDropEle = document.querySelector('.cover')
-function removeModal(event=None){
-    backDropEle.classList.add('display-none')
+function removeModal(event = None) {
+  backDropEle.classList.add('display-none')
 
 }
 
-function showModal(){
-    backDropEle.classList.remove('display-none')
+function showModal() {
+  backDropEle.classList.remove('display-none')
 }
-document.querySelector('.allocation-box .close-btn').addEventListener('click',removeModal)
-document.addEventListener("click", function (event){
+document.querySelector('.allocation-box .close-btn').addEventListener('click', removeModal)
+document.addEventListener("click", function (event) {
   const assignBtn = event.target.closest('.assign-btn')
-  if(!assignBtn) return
+  if (!assignBtn) return
   showModal()
- // console.log(assignBtn)
+  // console.log(assignBtn)
 })
 
-function insertRooms(number){ 
-let htmlString=``
+function insertRooms(number) {
+  let htmlString = ``
   for (var i = 0; i < number; i++) {
-    htmlString+=`
+    htmlString += `
     
-        <div class="student-card room-selection-card verified ${i==0?"active":""}">
+        <div class="student-card room-selection-card verified ${i == 0 ? "active" : ""}">
             <div class="card-header">
                 <div class="student-id-box">
                   <p class="dim-text">Room</p>
@@ -60,18 +60,21 @@ let htmlString=``
                </div>   
             
                 <div class="btns-box">
-                <button class='select-btn'>${i==0?"Selected":"Select Room"}</button>
+                <button class='select-btn'>${i == 0 ? "Selected" : "Select Room"}</button>
                 </div>
         </div>`
   }
-  document.querySelector(".rooms-box").innerHTML=htmlString
+  document.querySelector(".rooms-box").innerHTML = htmlString
 }
-insertRooms(4)
+insertRooms(5)
 
+// ALlocation Modal
+document.querySelector('.allocation-box')
+  .addEventListener('click', function (event) {
+    const selectBtn = event.target.closest('.room-selection-card .select-btn')
+    console.log(selectBtn)
+    if (!selectBtn) return
+    document.querySelector('.room-selection-card.active').classList.remove("active")
+    selectBtn.closest('.room-selection-card').classList.add("active")
+  })
 
-document.querySelector('.allocation-box', function(event){
-  const roomEle = event.target.closest('.room-selection-card')
-  console.log(roomEle)
-  if(!roomEle) return 
-roomEle.querySelector("button").classList.add("assign-btn")
-})

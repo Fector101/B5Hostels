066316ca9b23adc3e708e56ab061f92f7eab5e95
fector101 <fabianjoseph063@gmail.com>
2025-03-23 10:13:10 +0100

    Enhance tab functionality for student and room cards, update styles, and add hover effects

---
+++ public/js/admin-rooms.js
@@ -0,0 +1,32 @@
+
+function showTab(tab) {
+    // tab = 'pending' or 'verified' or 'paid' or ('student-card' <--- for all)
+    document.querySelectorAll('.student-card').forEach(card => {
+      if (!card.classList.contains(tab)) {
+        card.classList.add('display-none')
+      }else{
+        
+        card.classList.remove('display-none')
+      }
+    })
+    
+  }
+  
+  document.querySelector('.tabs').addEventListener('click', function (event) {
+    const tabBtn = event.target.closest('button')
+    this.querySelector('.active').classList.remove('active')
+    tabBtn.classList.add('active')
+    if (!tabBtn) return
+    if (tabBtn.innerText == 'Occupied') {
+      showTab('full')
+    } else if (tabBtn.innerText == 'Available') {
+      showTab('free')
+    }else if (tabBtn.innerText == 'Maintenance') {
+      showTab('maintenance')
+    }
+    else if (tabBtn.innerText == 'Paid') {
+      showTab('paid')
+    }else{
+      showTab('room-card')
+    }
+  })
--- public/js/admin-students.js
+++ public/js/admin-students.js
@@ -78,4 +78,32 @@ document.querySelector('.allocation-box')
     selectBtn.closest('.room-selection-card').classList.add("active")
   })
 
-// function show
+function showTab(tab) {
+  // tab = 'pending' or 'verified' or 'paid' or ('student-card' <--- for all)
+  document.querySelectorAll('.student-card').forEach(card => {
+    if (!card.classList.contains(tab)) {
+      card.classList.add('display-none')
+    }else{
+      
+      card.classList.remove('display-none')
+    }
+  })
+  
+}
+
+document.querySelector('.tabs').addEventListener('click', function (event) {
+  const tabBtn = event.target.closest('button')
+  this.querySelector('.active').classList.remove('active')
+  tabBtn.classList.add('active')
+  if (!tabBtn) return
+  if (tabBtn.innerText == 'Pending') {
+    showTab('pending')
+  } else if (tabBtn.innerText == 'Verified') {
+    showTab('verified')
+  }
+  else if (tabBtn.innerText == 'Paid') {
+    showTab('paid')
+  }else{
+    showTab('student-card')
+  }
+})

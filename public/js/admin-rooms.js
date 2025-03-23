a92bca9d805233db35fe6ba795866480c7e31d27
fector101 <fabianjoseph063@gmail.com>
2025-03-23 10:59:40 +0100

    broken

--- public/js/admin-rooms.js
+++ public/js/admin-rooms.js
@@ -1,15 +1,16 @@
 
 function showTab(tab) {
-    // tab = 'pending' or 'verified' or 'paid' or ('student-card' <--- for all)
-    document.querySelectorAll('.student-card').forEach(card => {
+    // tab = 'maintenance' or 'free' or 'full' or ('room-card' <--- for all)
+    
+    document.querySelectorAll('.room-card').forEach(card => {
         if (!card.classList.contains(tab)) {
             card.classList.add('display-none')
         } else {
-
             card.classList.remove('display-none')
         }
     })
-
+    const currentHostel = document.querySelector('.select-hostel').value
+    displayHostel(currentHostel)
 }
 
 document.querySelector('.tabs').addEventListener('click', function (event) {
@@ -17,16 +18,22 @@ document.querySelector('.tabs').addEventListener('click', function (event) {
     this.querySelector('.active').classList.remove('active')
     tabBtn.classList.add('active')
     if (!tabBtn) return
-    if (tabBtn.innerText == 'Occupied') {
-        showTab('full')
-    } else if (tabBtn.innerText == 'Available') {
-        showTab('free')
-    } else if (tabBtn.innerText == 'Maintenance') {
-        showTab('maintenance')
-    }
-    else if (tabBtn.innerText == 'Paid') {
-        showTab('paid')
-    } else {
-        showTab('room-card')
-    }
+    showTab(tabBtn.value)
+    
+})
+
+function displayHostel(hostel){
+    const currentTab = document.querySelector('.tabs button.active').value
+    document.querySelectorAll(`.cards-box > div.${currentTab}`).forEach(card => {
+        if (card.classList.contains(hostel)) {
+            card.classList.remove('display-none')
+        } else {
+            card.classList.add('display-none')
+        }
+    })
+}
+
+document.querySelector('.select-hostel').addEventListener('change', function () {
+    const selectedHostel = this.value
+    displayHostel(selectedHostel)
 })
--- public/js/admin-students.js
+++ public/js/admin-students.js
@@ -1,27 +1,28 @@
-
-const backDropEle = document.querySelector('.cover')
+const backDropEle = document.querySelector(".cover");
 function removeModal(event = None) {
-  backDropEle.classList.add('display-none')
-
+    backDropEle.classList.add("display-none");
 }
 
 function showModal() {
-  backDropEle.classList.remove('display-none')
+    backDropEle.classList.remove("display-none");
 }
-document.querySelector('.allocation-box .close-btn').addEventListener('click', removeModal)
+document
+    .querySelector(".allocation-box .close-btn")
+    .addEventListener("click", removeModal);
 document.addEventListener("click", function (event) {
-  const assignBtn = event.target.closest('.assign-btn')
-  if (!assignBtn) return
-  showModal()
-  // console.log(assignBtn)
-})
+    const assignBtn = event.target.closest(".assign-btn");
+    if (!assignBtn) return;
+    showModal();
+    // console.log(assignBtn)
+});
 
 function insertRooms(number) {
-  let htmlString = ``
-  for (var i = 0; i < number; i++) {
-    htmlString += `
+    let htmlString = ``;
+    for (var i = 0; i < number; i++) {
+        htmlString += `
     
-        <div class="student-card room-selection-card verified ${i == 0 ? "active" : ""}">
+        <div class="student-card room-selection-card verified ${i == 0 ? "active" : ""
+            }">
             <div class="card-header">
                 <div class="student-id-box">
                   <p class="dim-text">Room</p>
@@ -60,50 +61,61 @@ function insertRooms(number) {
                </div>   
             
                 <div class="btns-box">
-                <button class='select-btn'>${i == 0 ? "Selected" : "Select Room"}</button>
+                <button class='select-btn'>${i == 0 ? "Selected" : "Select Room"
+            }</button>
                 </div>
-        </div>`
-  }
-  document.querySelector(".rooms-box").innerHTML = htmlString
+        </div>`;
+    }
+    document.querySelector(".rooms-box").innerHTML = htmlString;
 }
-insertRooms(5)
+insertRooms(5);
 
 // ALlocation Modal
-document.querySelector('.allocation-box')
-  .addEventListener('click', function (event) {
-    const selectBtn = event.target.closest('.room-selection-card .select-btn')
-    console.log(selectBtn)
-    if (!selectBtn) return
-    document.querySelector('.room-selection-card.active').classList.remove("active")
-    selectBtn.closest('.room-selection-card').classList.add("active")
-  })
+document
+    .querySelector(".allocation-box")
+    .addEventListener("click", function (event) {
+        const selectBtn = event.target.closest(
+            ".room-selection-card .select-btn"
+        );
+        console.log(selectBtn);
+        if (!selectBtn) return;
+        document
+            .querySelector(".room-selection-card.active")
+            .classList.remove("active");
+        selectBtn.closest(".room-selection-card").classList.add("active");
+    });
 
 function showTab(tab) {
-  // tab = 'pending' or 'verified' or 'paid' or ('student-card' <--- for all)
-  document.querySelectorAll('.student-card').forEach(card => {
-    if (!card.classList.contains(tab)) {
-      card.classList.add('display-none')
-    } else {
-
-      card.classList.remove('display-none')
-    }
-  })
-
+    // tab = 'pending' or 'verified' or 'paid' or ('student-card' <--- for all)
+    document.querySelectorAll(".student-card").forEach((card) => {
+        if (!card.classList.contains(tab)) {
+            card.classList.add("display-none");
+        } else {
+            card.classList.remove("display-none");
+        }
+    });
+    const currentLevel = document.querySelector('.select-hostel').value
+    displayLevel(currentLevel)
 }
 
-document.querySelector('.tabs').addEventListener('click', function (event) {
-  const tabBtn = event.target.closest('button')
-  this.querySelector('.active').classList.remove('active')
-  tabBtn.classList.add('active')
-  if (!tabBtn) return
-  if (tabBtn.innerText == 'Pending') {
-    showTab('pending')
-  } else if (tabBtn.innerText == 'Verified') {
-    showTab('verified')
-  }
-  else if (tabBtn.innerText == 'Paid') {
-    showTab('paid')
-  } else {
-    showTab('student-card')
-  }
-})
+function displayLevel(hostel){
+    const currentTab = document.querySelector('.tabs button.active').value
+    const specificElement = document.querySelector('.cards-box > div');
+console.log(specificElement);
+
+    document.querySelectorAll(`.cards-box > div.[data-level="2"]`).forEach(card => {
+        if (card.classList.contains(hostel)) {
+            card.classList.remove('display-none')
+        } else {
+            card.classList.add('display-none')
+        }
+    })
+}
+
+document.querySelector(".tabs").addEventListener("click", function (event) {
+    const tabBtn = event.target.closest("button");
+    this.querySelector(".active").classList.remove("active");
+    tabBtn.classList.add("active");
+    if (!tabBtn) return;
+    showTab(tabBtn.value);
+});

5bd280b7ead6e7b6e40b7d579aa3ee7682da7d50
fector101 <fabianjoseph063@gmail.com>
2025-03-24 11:18:09 +0100

    useable

--- public/js/room.js
+++ public/js/room.js
@@ -1,7 +1,22 @@
+
 const make_payment_btn = document.querySelector('button#pay')
-make_payment_btn.addEventListener('click',function(){
+make_payment_btn.addEventListener('click', function () {
     document.querySelector('#room-page').classList.add('display-none')
     document.querySelector('#payment-page').classList.remove('display-none')
     // window.location.href = '/payment-portal'
 
 })
+
+document.getElementById("payment-form")
+    .addEventListener("submit", function (event) {
+        event.preventDefault();
+        document.querySelector('.confirm-payement-modal').classList.remove('display-none')
+        // setTimeout(() => {
+        // },1000)
+        setTimeout(() => {document.querySelector('.confirm-payement-modal .content-box').classList.remove('display-none')},1000)
+            // document.querySelector('.confirm-payement-modal').classList.add('display-none')
+            // document.querySelector('#payment-page').classList.add('display-none')
+            // document.querySelector('#room-page').classList.remove('display-none')
+        // }
+        //     , 1000)
+    })

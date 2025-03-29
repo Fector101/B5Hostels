d9488a3110cf9bceb548883c4b594431b8f1d6b9
fector101 <fabianjoseph063@gmail.com>
2025-03-29 23:32:39 +0100

    Refactor admin CSS for consistency and enhance Header component with responsive navigation

--- frontend/src/pages/admin-routes/Header.js
+++ frontend/src/pages/admin-routes/Header.js
@@ -1,19 +1,46 @@
 import { Building, Home, LogOut, Users } from "lucide-react";
+import { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 export default function Header() {
     const location = useLocation();
+    const [nav_state, setNavState] = useState(false)
+
+    useEffect(() => {
+        function handleResize() {
+            setNavState(false)
+            const signoutBtn = document.querySelector('.signout-btn')
+            const navEle = document.querySelector('header nav')
+            // if (window.innerWidth > 800) {
+                navEle.classList.remove('show-nav-phone')
+                signoutBtn.classList.remove('show-signout-btn')
+            // } 
+        }
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+
+
+
+    }, []);
+
+    function toggleNav() {
+        const signoutBtn = document.querySelector('.signout-btn')
+        const navEle = document.querySelector('header nav')        
+        navEle.classList.toggle('show-nav-phone')
+        signoutBtn.classList.toggle('show-signout-btn')
+        setNavState(old => !old)
+    }
     return (
         <header className="admin-header">
             <div className="menu-btn-box">
                 <h1>Hostel Manager</h1>
-                <button className="menu-btn">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="menu-svg" data-lov-id="src/components/Navbar.tsx:62:56" data-lov-name="Menu" data-component-path="src/components/Navbar.tsx" data-component-line="62" data-component-file="Navbar.tsx" data-component-name="Menu" data-component-content="%7B%22className%22%3A%22h-5%20w-5%22%7D"><line x1="4" x2="20" y1="12" y2="12"></line><line x1="4" x2="20" y1="6" y2="6"></line><line x1="4" x2="20" y1="18" y2="18"></line></svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="x-svg display-none" data-lov-id="src/components/Navbar.tsx:62:28" data-lov-name="X" data-component-path="src/components/Navbar.tsx" data-component-line="62" data-component-file="Navbar.tsx" data-component-name="X" data-component-content="%7B%22className%22%3A%22h-5%20w-5%22%7D"><path d="M18 6 6 18"></path><path d="m6 6 12 12"></path></svg>
+                <button className="menu-btn" onClick={toggleNav}>
+                    <svg className={"menu-svg" + (nav_state ? ' display-none' : '')} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" data-lov-id="src/components/Navbar.tsx:62:56" data-lov-name="Menu" data-component-path="src/components/Navbar.tsx" data-component-line="62" data-component-file="Navbar.tsx" data-component-name="Menu" data-component-content="%7B%22className%22%3A%22h-5%20w-5%22%7D"><line x1="4" x2="20" y1="12" y2="12"></line><line x1="4" x2="20" y1="6" y2="6"></line><line x1="4" x2="20" y1="18" y2="18"></line></svg>
+                    <svg className={"x-svg" + (!nav_state ? ' display-none' : '')} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" data-lov-id="src/components/Navbar.tsx:62:28" data-lov-name="X" data-component-path="src/components/Navbar.tsx" data-component-line="62" data-component-file="Navbar.tsx" data-component-name="X" data-component-content="%7B%22className%22%3A%22h-5%20w-5%22%7D"><path d="M18 6 6 18"></path><path d="m6 6 12 12"></path></svg>
                 </button>
             </div>
             <nav>
-                <Link style={{ width: "109px" }}
+                <Link style={{ width: "110px" }}
                     className={location.pathname.endsWith('dashboard') ? 'active' : ''}
                     to="/admin/dashboard"
                 >
